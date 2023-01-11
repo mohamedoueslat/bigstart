9fdef55df7bf825e38a9d02be627a909344793b4
Mohame0136 <mohame0136@example.com>
2023-01-11 16:36:32 +0100

    first commit

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'mohamed oueslati',
+  email: 'rise@project.com',
+  photoURL: '/assets/images/avatars/avatar_default.jpg',
+};
+
+export default account;
---
+++ src/components/logo/Logo.js
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import { forwardRef } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+// @mui
+import { useTheme } from '@mui/material/styles';
+import { Box, Link } from '@mui/material';
+import p2 from "./p2.png"
+
+// ----------------------------------------------------------------------
+
+const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
+  const theme = useTheme();
+
+  const PRIMARY_LIGHT = theme.palette.primary.light;
+
+  const PRIMARY_MAIN = theme.palette.primary.main;
+
+  const PRIMARY_DARK = theme.palette.primary.dark;
+
+  // OR using local (public folder)
+  // -------------------------------------------------------
+  const logo = (
+    <Box
+      component="img"
+      src={p2}
+      sx={{ width: 170, height: 50, cursor: 'pointer', ...sx }}
+    />
+  );
+
+
+  
+
+
+  if (disabledLink) {
+    return <>{logo}</>;
+  }
+
+  return (
+    <Link to="/" component={RouterLink} sx={{ display: 'contents' }}>
+      {logo}
+    </Link>
+  );
+});
+
+Logo.propTypes = {
+  sx: PropTypes.object,
+  disabledLink: PropTypes.bool,
+};
+
+export default Logo;
